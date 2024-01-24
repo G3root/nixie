ea8015e5dde2e4c53596cda9e0e3263e4d901372
nafees nazik <nafeesnazik21@gmail.com>
2024-01-24 18:55:47 +0530

    feat: add nano id

---
+++ app/utils/id.ts
@@ -0,0 +1,15 @@
+import { customAlphabet } from 'nanoid'
+
+const customIdChar = customAlphabet(
+  '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+)
+
+const prefixes = {
+  user: 'usr',
+  totp: 'totp',
+  test: 'test', // for tests only
+} as const
+
+export function newId(prefix: keyof typeof prefixes) {
+  return `${prefixes[prefix]}_${customIdChar(18)}`
+}
