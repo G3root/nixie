c481cc98dedfb1e7a457b20b22c8fccb19826b37
nafees nazik <nafeesnazik21@gmail.com>
2024-02-01 01:38:17 +0530

    chore: rename action

--- app/server/routers/action/contact/index.ts
+++ app/server/routers/action/contact/index.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter } from '~/server/trpc'
 import { createContact } from './create-create'
 
-export const contact = createTRPCRouter({
+export const contactAction = createTRPCRouter({
   create: createContact,
 })
