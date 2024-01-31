9a07bf59497b02db900c66b70ff282a6fb6c9289
nafees nazik <nafeesnazik21@gmail.com>
2024-02-01 01:38:45 +0530

    feat: add contacts page

---
+++ app/features/contacts/components/contacts-table/index.tsx
@@ -0,0 +1,206 @@
+import * as React from 'react'
+import {
+  type ColumnDef,
+  type ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  type SortingState,
+  useReactTable,
+  type VisibilityState,
+} from '@tanstack/react-table'
+import { Button } from '~/components/ui/button'
+import { Checkbox } from '~/components/ui/checkbox'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu'
+import { Icon } from '~/components/ui/icon'
+import { Input } from '~/components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/ui/table'
+import { type RouterOutputs } from '~/trpc/shared'
+
+export type Item = RouterOutputs['loader']['contact']['all']['data'][number]
+
+export const columns: ColumnDef<Item>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={value => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <Icon name="chevrons-up-down" className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue('name')}</div>,
+  },
+  {
+    accessorKey: 'relation',
+    header: () => <div className="text-right">Relation</div>,
+    cell: ({ row }) => {
+      return (
+        <div className="text-right font-medium">{row.getValue('relation')}</div>
+      )
+    },
+  },
+]
+
+interface ContactsTableProps {
+  data: RouterOutputs['loader']['contact']['all']['data']
+}
+
+export function ContactsTable({ data }: ContactsTableProps) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  )
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter emails..."
+          value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
+          onChange={event =>
+            table.getColumn('email')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <Icon name="chevron-down" className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter(column => column.getCanHide())
+              .map(column => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={value => column.toggleVisibility(!!value)}
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                )
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
--- app/routes/dashboard+/contacts_+/_index/page.tsx
+++ app/routes/dashboard+/contacts_+/_index/page.tsx
@@ -1,3 +1,32 @@
+import { type LoaderFunctionArgs } from '@remix-run/node'
+import { Link } from '@remix-run/react'
+import { typedjson, useTypedLoaderData } from 'remix-typedjson'
+import { buttonVariants } from '~/components/ui/button'
+import { Separator } from '~/components/ui/separator'
+import { ContactsTable } from '~/features/contacts/components/contacts-table'
+import { trpc } from '~/trpc/trpc.server'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const data = await trpc({ request }).loader.contact.all.query()
+
+  return typedjson(data)
+}
+
 export default function ContactsPage() {
-  return <div>contacts</div>
+  const data = useTypedLoaderData<typeof loader>()
+  return (
+    <div>
+      <div className="flex items-center justify-between pb-8 pt-4">
+        <h3 className="text-3xl font-bold tracking-tight">Contacts</h3>
+
+        <Link to="create" className={buttonVariants()}>
+          Create
+        </Link>
+      </div>
+
+      <Separator />
+
+      <ContactsTable data={data.data} />
+    </div>
+  )
 }
