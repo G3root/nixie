4371abf419fadce51a961d68245f14d7acdc49c2
nafees nazik <nafeesnazik21@gmail.com>
2024-02-01 01:21:35 +0530

    feat: add use toast hook

--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -1,6 +1,21 @@
-import { Toaster as Sonner } from 'sonner'
+import { useEffect } from 'react'
+import { toast as showToast, Toaster as Sonner } from 'sonner'
+import { type Toast } from '~/utils/toast.server'
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
+
+export function useToast(toast?: Toast | null) {
+  useEffect(() => {
+    if (toast) {
+      setTimeout(() => {
+        showToast[toast.type](toast.title, {
+          id: toast.id,
+          description: toast.description,
+        })
+      }, 0)
+    }
+  }, [toast])
+}
 
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
