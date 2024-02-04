85a821e51e9414f6f508dda8c2852826a40b6ad8
nafees nazik <nafeesnazik21@gmail.com>
2024-02-04 23:00:51 +0530

    fix: query

--- app/server/routers/loader/contact/all-contacts.ts
+++ app/server/routers/loader/contact/all-contacts.ts
@@ -1,10 +1,20 @@
+import { jsonArrayFrom } from 'kysely/helpers/sqlite'
 import { protectedProcedure } from '~/server/trpc'
 
 export const allContacts = protectedProcedure.query(async ({ ctx, input }) => {
   const { db, user } = ctx
 
   const data = await db.kysely
-    .selectFrom('contact')
+    .selectFrom('contact as c')
+    .select(eb => [
+      'id',
+      jsonArrayFrom(
+        eb
+          .selectFrom('email as e')
+          .select(['e.id', 'e.label', 'e.value'])
+          .whereRef('e.contactId', '=', 'c.id'),
+      ).as('email'),
+    ])
     .where('ownerId', '==', user.id)
     .selectAll()
     .execute()
